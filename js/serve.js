1c971e47167d3599f1348baf83f3a5169a4d1004
Shea Duma <nu@she-a.eu>
2025-07-20 03:34:11 +0300

    added a cookie value

--- js/serve.js
+++ js/serve.js
@@ -1,1 +1,22 @@
-console.log("uwu");
+status = "none";
+downloadButton = document.getElementById("Middle"); 
+cookieText = document.getElementById("cookieText"); 
+
+status = document.cookie.substring(document.cookie.search("status=")+7);
+
+function updateCookieText(){
+    cookieText.innerHTML = "<p>path=" + location.pathname.substring(1) + "</p><p>cookie=" + status+"</p>";
+} updateCookieText();
+
+function downloadButtonClick(){
+    switch(String(status)){
+        case "none": status = "pressed"; break;
+        case "pressed": status = "retry"; break;
+        case "retry": status = "none"; break;
+        default: status = "broken"; break;
+    }
+    document.cookie = "status="+status;
+    updateCookieText();
+}
+
+downloadButton.addEventListener('click', downloadButtonClick);
