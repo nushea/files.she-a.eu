ed67c30461572d75a37bc26c5d827aa5da2f99fa
Shea Duma <nu@she-a.eu>
2025-07-20 04:09:03 +0300

    base config

--- js/serve.js
+++ js/serve.js
@@ -3,17 +3,31 @@ downloadButton = document.getElementById("Middle");
 cookieText = document.getElementById("cookieText"); 
 
 status = document.cookie.substring(document.cookie.search("status=")+7);
+if(status != "none" && status != "pressed" && status != "retry")
+    status = "none";
+
+
+function startDownload(){
+    console.log("downloathe");
+}
 
 function updateCookieText(){
-    cookieText.innerHTML = "<p>path=" + location.pathname.substring(1) + "</p><p>cookie=" + status+"</p>";
+    cookieText.innerHTML = "<p>You are about to download the file: " + location.pathname.substring(1) + "</p>";
+    switch(String(status)){
+        case "none":    cookieText.innerHTML += "<p> Press anywhere in the middle part of the website to download the document</p>"; break;
+        case "pressed": cookieText.innerHTML += "<p> You are downloading the document. </p><p> Press press again thrice to download again </p>"; break;
+        case "retry":   cookieText.innerHTML += "<p> You are reseting the cookie, </p><p> after that you can press once more </p>"; break;
+        default: location.reload(); break;
+    }
+    
 } updateCookieText();
 
 function downloadButtonClick(){
     switch(String(status)){
-        case "none": status = "pressed"; break;
+        case "none": status = "pressed"; startDownload(); break;
         case "pressed": status = "retry"; break;
         case "retry": status = "none"; break;
-        default: status = "broken"; break;
+        default: location.reload(); break;
     }
     document.cookie = "status="+status;
     updateCookieText();
