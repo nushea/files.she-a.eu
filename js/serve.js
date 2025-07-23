04af865e9929880f35de0973032c8924e07ebf1f
Shea Duma <nu@she-a.eu>
2025-07-23 07:03:55 +0300

    redone the whole layout

--- js/serve.js
+++ js/serve.js
@@ -1,17 +1,22 @@
+document.getElementById("fields").style.position = "unset";
+document.getElementById("fields").style.left = "unset";
+
 status = "none";
-downloadButton = document.getElementById("Middle"); 
+downloadButton = document.getElementById("itemButton"); 
 cookieText = document.getElementById("cookieText"); 
 
 status = document.cookie.substring(document.cookie.search("status=")+7);
-if(status != "none" && status != "pressed" && status != "retry")
+if(status != "pressed")
     status = "none";
 
+if(location.pathname.length > 1){
+    document.getElementById("key").value = location.pathname.substring(1, location.pathname.indexOf('&'));
+    document.getElementById("pas").value = location.pathname.substring(1+ location.pathname.indexOf('&'));
+}
 
-function startDownload() {
-//    window.location = "https://files.she-a.eu/fileServer/" + location.pathname;
+function startDownload(key, pass) {
     givenName = "hmmm";
-    const url = 'https://files.she-a.eu/fileServer' + location.pathname;
-    console.log(url);
+    const url = 'https://files.she-a.eu/fileServer/' + key + '&' + pass;
     fetch(url)
         .then(res => {
             if (!res.ok) throw new Error("download failed");
@@ -29,29 +34,43 @@ function startDownload() {
             document.body.removeChild(a);
             URL.revokeObjectURL(a.href);
         })
-        .catch(err => console.error("download error:", err));
+        .catch(err => console.log("nop"));
 }
 
 function updateCookieText(){
-    cookieText.innerHTML = "<p>You are about to download the file: " + location.pathname.substring(1) + "</p>";
+    cookieText.innerHTML = "<p>Please fill in the fields below:</p>";
+//    console.log(Date.now()/1000);
     switch(String(status)){
-        case "none":    cookieText.innerHTML += "<p> Press anywhere in the middle part of the website to download the document</p>"; break;
-        case "pressed": cookieText.innerHTML += "<p> You are downloading the document. </p><p> Press press again thrice to download again </p>"; break;
-        case "retry":   cookieText.innerHTML += "<p> You are reseting the cookie, </p><p> after that you can press once more </p>"; break;
+        case "none":    cookieText.innerHTML += "<p> Download</p>"; break;
+        case "newPress": cookieText.innerHTML += "<p> Downloading </p>"; break;
+        case "pressed": cookieText.innerHTML += "<p> Already Downloaded </p>"; break;
         default: location.reload(); break;
     }
     
 } updateCookieText();
 
-function downloadButtonClick(){
-    switch(String(status)){
-        case "none": status = "pressed"; startDownload(); break;
-        case "pressed": status = "retry"; break;
-        case "retry": status = "none"; break;
-        default: location.reload(); break;
+function downloadButtonClick(key, pass){
+    if(key == ""){
+        document.getElementById("key").placeholder = "Name (REQUIRED)";
+        document.getElementById("key").style.color = "#f38ba8";
+        return;
     }
-    document.cookie = "status="+status;
+    if(status == "none")
+        status = "newPress";
+    downloadButton.disabled = true;
+    units = 400;
+    const countdown = setInterval(() => {
+        units -= 1;
+        if (units > 0) {
+            downloadButton.textContent = "FETCH ("+ (units/100).toFixed(2) +"s)";
+        } else {
+            clearInterval(countdown);
+            downloadButton.disabled = false;
+            downloadButton.textContent = "FETCH";
+        }
+    }, 10);
+    startDownload(key, pass);
+    document.cookie = "status=pressed";
     updateCookieText();
 }
 
-downloadButton.addEventListener('click', downloadButtonClick);
